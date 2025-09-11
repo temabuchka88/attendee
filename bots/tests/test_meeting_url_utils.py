e7684bf6ec3d4f681cbc4fe57f0935161a8036e9
Noah Duncan <crusty.noah@gmail.com>
2025-09-11 18:33:01 -0500

    lint

--- bots/tests/test_meeting_url_utils.py
+++ bots/tests/test_meeting_url_utils.py
@@ -1,9 +1,10 @@
 # test_meeting_utils.py
+import base64
+import json
 import unittest
 
 from bots.meeting_url_utils import MeetingTypes, domain_and_subdomain_from_url, meeting_type_from_url, normalize_meeting_url, root_domain_from_url
-import base64
-import json
+
 
 class TestMeetingUrlUtils(unittest.TestCase):
     def test_root_domain_from_url(self):
@@ -35,19 +36,18 @@ class TestMeetingUrlUtils(unittest.TestCase):
         self.assertEqual(normalize_meeting_url("https://zoom.us/j/123456789")[1], "https://zoom.us/j/123456789")
         self.assertEqual(normalize_meeting_url("zoom.us/j/123456789")[1], "https://zoom.us/j/123456789")
         self.assertEqual(normalize_meeting_url("https://meet.google.com/abc-defg-hij")[1], "https://meet.google.com/abc-defg-hij")
-        
+
         self.assertEqual(normalize_meeting_url("meet.google.com/abc-defg-hij")[1], "https://meet.google.com/abc-defg-hij")
         self.assertEqual(normalize_meeting_url("https://teams.microsoft.com/l/meetup-join/19%3ameeting_NjnnnnnnnnnnnnnnnnnnnnnnnnnnMzctODQwYTBmMDQ4MzQ2%40thread.v2/0?context=%7b%22Tid%22%3a%22b8291b4b-aaaa-bbbb-8a00-9d5fc37b9a77%22%2c%22Oid%22%3a%22216d2e11-45cc-9999-9689-d05554e5c1d1%22%7d>")[1], 'https://teams.microsoft.com/l/meetup-join/19:meeting_NjnnnnnnnnnnnnnnnnnnnnnnnnnnMzctODQwYTBmMDQ4MzQ2@thread.v2/0?context={"Tid":"b8291b4b-aaaa-bbbb-8a00-9d5fc37b9a77","Oid":"216d2e11-45cc-9999-9689-d05554e5c1d1"}')
-        self.assertEqual(normalize_meeting_url("https://teams.microsoft.com/meet/999999999999?p=aaaaalRE2XBPsAr00W")[1], 'https://teams.microsoft.com/meet/999999999999?p=aaaaalRE2XBPsAr00W')
+        self.assertEqual(normalize_meeting_url("https://teams.microsoft.com/meet/999999999999?p=aaaaalRE2XBPsAr00W")[1], "https://teams.microsoft.com/meet/999999999999?p=aaaaalRE2XBPsAr00W")
 
-        self.assertEqual(normalize_meeting_url("https://teams.live.com/meet/999999999999?p=aaaaalRE2XBPsAr00W")[1], 'https://teams.live.com/meet/999999999999?p=aaaaalRE2XBPsAr00W')
+        self.assertEqual(normalize_meeting_url("https://teams.live.com/meet/999999999999?p=aaaaalRE2XBPsAr00W")[1], "https://teams.live.com/meet/999999999999?p=aaaaalRE2XBPsAr00W")
         self.assertEqual(normalize_meeting_url("https://teams.microsoft.com/dl/launcher/launcher.html?url=/_#/l/meetup-join/19:meeting_NDQ3Y2Q1NDEtY2I5Ni00MzEyLTgzfffffffffffffffffffffffffff@thread.v2/0?context=%7b%22Tid%22%3a%22b00367e2-aaaa-bbbb-cccc-7245d45c0947%22%2c%22Oid%22%3a%2266666666-3a9d-441a-892d-55555555555%22%7d&anon=true&type=meetup-join&deeplinkId=5044eb28-9d33-4309-bf23-3aa618a9e6b0&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true")[1], 'https://teams.microsoft.com/l/meetup-join/19:meeting_NDQ3Y2Q1NDEtY2I5Ni00MzEyLTgzfffffffffffffffffffffffffff@thread.v2/0?context={"Tid":"b00367e2-aaaa-bbbb-cccc-7245d45c0947","Oid":"66666666-3a9d-441a-892d-55555555555"}')
-        coord_json = {"conversationId":"19:meeting_ffffffffffffffffffffffffffffffffffffffffffffffff@thread.v2","tenantId":"ffffffff-ffff-fff-ffff-6866ff300052","organizerId":"ffffffff-ffff-4085-acbd-ffffffffffff","messageId":"0"}
+        coord_json = {"conversationId": "19:meeting_ffffffffffffffffffffffffffffffffffffffffffffffff@thread.v2", "tenantId": "ffffffff-ffff-fff-ffff-6866ff300052", "organizerId": "ffffffff-ffff-4085-acbd-ffffffffffff", "messageId": "0"}
         fake_coord = base64.b64encode(json.dumps(coord_json).encode("utf-8"))
         # Drop trailing equals
         fake_coord = fake_coord.decode("utf-8").rstrip("=")
-        self.assertEqual(normalize_meeting_url(f"https://teams.microsoft.com/light-meetings/launch?agent=web&version=25072001100&coords={fake_coord}%3D&deeplinkId=0ac0a241-11111-111-8ddc-4a4b333dd286&correlationId=1111111-1111-1111-1111-11111111111")[1], 
-        "https://teams.microsoft.com/l/meetup-join/" + coord_json['conversationId'] + f"/0?context={json.dumps({'Tid': coord_json['tenantId'], 'Oid': coord_json['organizerId']}, separators=(',', ':'))}")
+        self.assertEqual(normalize_meeting_url(f"https://teams.microsoft.com/light-meetings/launch?agent=web&version=25072001100&coords={fake_coord}%3D&deeplinkId=0ac0a241-11111-111-8ddc-4a4b333dd286&correlationId=1111111-1111-1111-1111-11111111111")[1], "https://teams.microsoft.com/l/meetup-join/" + coord_json["conversationId"] + f"/0?context={json.dumps({'Tid': coord_json['tenantId'], 'Oid': coord_json['organizerId']}, separators=(',', ':'))}")
 
     def test_meeting_type_from_url(self):
         self.assertEqual(meeting_type_from_url("https://zoom.us/j/123456789"), MeetingTypes.ZOOM)
